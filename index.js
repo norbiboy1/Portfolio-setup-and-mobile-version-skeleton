616cc25be34b8ab7011a21f91bda649f10469ad8
businesskaniki <businesskaniki@gmail.com>
2022-06-20 16:55:07 +0300

    added the js functionality

---
+++ index.js
@@ -0,0 +1,17 @@
+const hamburger =document.querySelector('.hamburger-icon');
+const nav_menu = document.querySelector('nav');
+
+// hamburger.addEventListener("click", ()=>{
+//     hamburger.classList.toggle("hamburger-icon");
+//     nav_menu.classList.toggle("nav");
+//     console.log(hamburger);
+
+// })
+
+function onClick (){
+    hamburger.style.display='block';
+    nav_menu.style.display='none';
+    if(nav_menu.style.display=='none'){
+        var humburger_
+    }
+    console.log(hamburger)
