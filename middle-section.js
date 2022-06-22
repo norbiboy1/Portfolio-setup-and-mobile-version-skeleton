f2665906dcf4a49b01d5c0f95f45ca0d83d3c21b
norbiboy1 <norbiateniszes@yahoo.com>
2022-06-22 16:09:56 +0300

    I put the main.js to the end of the body, I add blur to background.

--- middle-section.js
+++ middle-section.js
@@ -1,11 +1,12 @@
-let works = document.querySelector('.works');
-let cardWorks = document.querySelector('.card-works-1');
-let cardWorksReverse = document.querySelector('.card-works-1 row-reverse');
-let photo = document.querySelector('.snapshoot-portofolio-1');
-let blocks = document.querySelector('.block-1');
-let buttonX = document.querySelector('.Close-btn');
-
+/*
+const works = document.querySelector('.works');
+const cardWorks = document.querySelector('.card-works-1');
+const cardWorksReverse = document.querySelector('.card-works-1 row-reverse');
+const photo = document.querySelector('.snapshoot-portofolio-1');
+const blocks = document.querySelector('.block-1');
+const buttonX = document.querySelector('.Close-btn');
 
 buttonX.addEventListener('click', () => {
-   works.classList.toggle('active');
+  works.classList.toggle('active');
 });
+*/
